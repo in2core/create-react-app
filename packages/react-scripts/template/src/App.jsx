6067f26dbd486ced7396bdf369c1e2803067e481
Radovan Kuka <kuka.radovan@gmail.com>
2017-04-12 13:16:44 +0200

    fix typo

--- packages/react-scripts/template/src/App.jsx
+++ packages/react-scripts/template/src/App.jsx
@@ -5,12 +5,12 @@ import styles from './App.css';
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <div className={styles.App-header}>
-          <img src={logo} className={styles.App-logo} alt="logo" />
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className={styles.App-intro>
+        <p className={styles.appIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
